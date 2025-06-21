3fe26c900ffa8100f53fd0e1073ec7f370f9a1a7
unknown <muhammadirfan54369@gmail.com>
2025-06-21 09:51:08 +0500

    Complete project added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./globals.css";
 import ChangeTitle from "./ChangeTitle";
+import Userdata from "./User";
+import User from "./User";
 
 const App = () => {
   return (
     <>
-      <ChangeTitle />
+      {/* <ChangeTitle /> */}
+      <User />
     </>
   );
 };
